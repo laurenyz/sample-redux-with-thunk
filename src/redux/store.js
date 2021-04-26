afd17cc3c754fcf21ea0849805b069fd68ab9200
Lauren Yu <lauren.yu@datasociety.com>
2021-04-26 17:22:46 -0400

    installed react-thunk and compose to aenable redux dev tools

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,13 @@
-import { createStore } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
 import rootReducer from './reducer'
+import thunk from 'redux-thunk'
+import { compose } from 'redux'
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const store = createStore(
     rootReducer,
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    composeEnhancers(applyMiddleware(thunk))
 );
 
 export default store;
